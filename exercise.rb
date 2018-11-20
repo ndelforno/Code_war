2ac43b8cb97614ee5db6f064a583df93279d84ae
Ndelforno <nicolas.delforno@gmail.com>
2018-11-20 16:29:01 -0500

    first exercise done

---
+++ exercise.rb
@@ -0,0 +1,8 @@
+#Complete the method/function so that it converts dash/underscore delimited words into camel casing. The first word within the output should be capitalized only if the original word was capitalized.
+def to_camel_case(str)
+  if str.include?('_')
+    str.split('_').inject([]){ |buffer,e| buffer.push(buffer.empty? ? e : e.capitalize) }.join
+  else
+    str.split('-').inject([]){ |buffer,e| buffer.push(buffer.empty? ? e : e.capitalize) }.join
+  end
+end
